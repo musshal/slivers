675708075afb5e71769948d2e3b0af1f03bcbf4a
Musthafa Faishal <musthafafaishal@gmail.com>
2023-08-01 00:14:47 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/dicoding/slivers/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dicoding.slivers
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:slivers/pixel_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Google Pixel',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const PixelPage(),
+    );
+  }
+}
